045b10cf3be512470db7ba64b5b1de55f89656b9
Hongbusi <q951686662@qq.com>
2022-01-16 23:20:57 +0800

    chore: webpack-server

---
+++ 07_webpack-server/src/js/element.js
@@ -0,0 +1,27 @@
+import '../css/style.css';
+import '../css/title.less';
+import '../css/image.css';
+import '../font/iconfont.css';
+
+import logo from '../img/logo.png';
+
+const divEl = document.createElement('div');
+divEl.className = 'title';
+divEl.innerHTML = 'Hello World';
+
+// 设置背景图片
+const bgDivEl = document.createElement('div');
+bgDivEl.className = 'image-bg';
+
+// 设置 img 元素的 src
+const imgEl = document.createElement('img');
+imgEl.src = logo;
+
+// i 元素
+const iEl = document.createElement('i');
+iEl.className = 'iconfont icon-ashbin';
+
+document.body.appendChild(divEl);
+document.body.appendChild(bgDivEl);
+document.body.appendChild(imgEl);
+document.body.appendChild(iEl);
---
+++ 07_webpack-server/src/main.js
@@ -0,0 +1,9 @@
+import './js/element';
+import './js/test';
+
+import App from './App.vue';
+import { createApp } from 'vue';
+
+const app = createApp(App);
+
+app.mount('#app');
