9dde80eba1f5792ffe6ae5f045c0b96aa3f79992
Hongbusi <q951686662@qq.com>
2022-01-16 19:02:53 +0800

    chore: CleanWebpackPlugin

--- 04_webpack-plugin/webpack.config.js
+++ 04_webpack-plugin/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -37,5 +38,8 @@ module.exports = {
         }
       }
     ]
-  }
+  },
+  plugins: [
+    new CleanWebpackPlugin()
+  ]
 }
