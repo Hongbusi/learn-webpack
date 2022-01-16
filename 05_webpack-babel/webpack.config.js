57dfdc379f6e3a05e316c641c9febd92f76def16
Hongbusi <q951686662@qq.com>
2022-01-16 22:15:02 +0800

    chore: webpack-babel

---
+++ 05_webpack-babel/webpack.config.js
@@ -0,0 +1,74 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { DefinePlugin } = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  // 设置模式
+  // development
+  // production
+  mode: 'development',
+  // 设置 source-map，建立 js 映射文件，方便调试代码和错误
+  devtool: 'source-map',
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'js/build.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(css|less)$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'less-loader',
+          'postcss-loader'
+        ]
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        type: 'asset',
+        generator: {
+          filename: 'img/[name]-[hash:6][ext]'
+        },
+        parser: {
+          dataUrlCondition: {
+            maxSize: 10 * 1024
+          }
+        }
+      },
+      {
+        test: /\.(eot|ttf|woff2?)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'font/[name]-[hash:6][ext]'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      title: 'Hongbusi'
+    }),
+    new DefinePlugin({
+      BASE_URL: '"./"'
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'public',
+          to: '.',
+          globOptions: {
+            ignore: [
+              '**/index.html'
+            ]
+          }
+        }
+      ]
+    })
+  ]
+}
