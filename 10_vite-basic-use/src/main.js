53880009c385c665ce4cbe861fb43a7f99b934a7
Hongbusi <q951686662@qq.com>
2022-01-17 23:25:16 +0800

    chore: vite

--- 10_vite-basic-use/src/main.js
+++ 10_vite-basic-use/src/main.js
@@ -1,6 +1,13 @@
-import _ from '../node_modules/lodash-es/lodash.default.js';
-import { sum } from './js/math.js';
+import _ from 'lodash-es';
+import { sum } from './js/math';
+
+import './css/style.css';
+import './css/title.less';
 
 console.log(sum(10, 20));
 
 console.log(_.join(['a', 'b', 'c']));
+
+const titleEl = document.createElement('div');
+titleEl.className = 'title';
+document.body.appendChild(titleEl);
